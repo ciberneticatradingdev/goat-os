7f5f7f6e99727f492536b80885bb8ff5dd294fca
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2024-08-26 04:56:13 -0400

    batalla 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://astro-simple-starter.netlify.app/",
   integrations: [tailwind(), sitemap(), solidJs()],
   output: "server", // o "server", según el caso
+  adapter: vercel(),
   ssr: true,
 });
