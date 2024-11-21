7bce6f92b37913b445167528553017578fc3cce9
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2024-11-21 07:21:47 -0300

    act 1, first version

--- src/pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const POST: APIRoute = async ({ request }) => {
-  const rawBody = await request.text();
-  console.log('Raw Request Body:', rawBody);
-  return new Response(JSON.stringify({ body: rawBody }), { status: 200 });
-};
